1e15a266fc15df03ccbc7829abd69d5667720787
Eduardo Moreno <emoreno@uchicago.edu>
2015-08-03 15:57:32 -0700

    association working

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ app.set('view engine', 'ejs');
 // a "GET" request to "/" will run the function below
 app.get("/", function (req, res) {
   // send back the response: 'Hello World'
-  
-  db.findAll({}).then(function(Users){
-  	console.log(Users[0].dataValues)
-  	res.render('index', {Users:Users});
+  db.Location.findAll({}).then(function(Locations){
+  	console.log(Locations[0].getReviews())
+  	     Locations[0].getReviews().then(function (rev){ 
+  			 console.log(rev[0].date)  
+  			 rev[0].date  
+  			 res.render('index', {Locations:Locations, rev:rev});
+  	     }) 
+  	
   });
 
 });
--- models/index.js
+++ models/index.js
@@ -12,7 +12,112 @@ var sequelize = new Sequelize('playdb', 'eduardo', '', {
   
 });
 
+var Location = sequelize.define('location', {
+  url_yelp: {
+    type: Sequelize.STRING,
+    unique: true
+  },
+  name: {
+    type: Sequelize.STRING
+  },
+  address: {
+    type: Sequelize.STRING
+  },
+  phone: {
+    type: Sequelize.STRING
+  },
+  rating: {
+    type: Sequelize.DOUBLE
+  },
+  review_count: {
+    type: Sequelize.INTEGER
+  },
+  price: {
+    type: Sequelize.STRING
+  },
+  url: {
+    type: Sequelize.STRING
+  },
+  senti_total: {
+    type: Sequelize.DOUBLE
+  },
+  senti_ave: {
+    type: Sequelize.DOUBLE
+  },
+  senti_max: {
+    type: Sequelize.DOUBLE
+  },
+  senti_min: {
+    type: Sequelize.DOUBLE
+  },
+  category: {
+    type: Sequelize.STRING
+  },
+  lat: {
+    type: Sequelize.DOUBLE
+  },
+  long: {
+    type: Sequelize.DOUBLE
+  },
+  our_rating: {
+    type: Sequelize.DOUBLE
+  }
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
+
+var Review = sequelize.define('review', {
+  date: {
+    type: Sequelize.STRING
+  },
+  user_id: {
+    type: Sequelize.STRING
+  },
+  user_review_count: {
+    type: Sequelize.INTEGER
+  },
+  user_friend_count: {
+    type: Sequelize.INTEGER
+  },
+  rating: {
+    type: Sequelize.DOUBLE
+  },
+  review: {
+    type: Sequelize.TEXT
+  },
+  review_length: {
+    type: Sequelize.INTEGER
+  }
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
+
+// Review.belongsTo(Location)
+Location.hasMany(Review)
+
+// Location.hasMany(Review)
+
+Location.sync({force: true}).then(function () {
+
+	Location.create({
+	  url_yelp: 'test location'
+	});
+ 
+  	Review.sync({force: true}).then(function () {
+ 
+   		return Review.create({
+   			date: 'test review',
+   			locationId: 1
+   		});
+  	});
+
+});
+
+// Location.hasMany(Review)
+
+// module.exports = Location;
+
 module.exports = sequelize;
 
-module.exports.Location = require("./location");
-module.exports.Review = require("./review");
+module.exports.Location = Location;
+module.exports.Review = Review;
