68a193d2b6262ecf79ccc463ce3462ad6d88821c
Eduardo Moreno <emoreno@uchicago.edu>
2015-08-03 23:44:52 -0700

    moves scrape functions t o app.js instead of scrape.js, needs to figure out require, promises and repetition of same url

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var express = require('express'),
   app = express();
 
 var db = require('./models')
+var request = require('request');
+var cheerio = require('cheerio');
+
+
+var baseUrl = 'http://www.yelp.com/'
+var addUrl1 = 'search?find_desc=&find_loc=San+Francisco%2C+CA&ns=1#find_desc=food'
+var searchResultsCounter = 0;
+var locations = []
 
 var scrape = require('./scrape')
 
@@ -12,34 +20,15 @@ app.get("/", function (req, res) {
   // send back the response: 'Hello World'
   
 
-  scrape.getLocationUrls().then(function(locations){
-    console.log('app.js!!!!!!!!!!!!!! ' + locations)
-  })
+  // scrape.getLocationUrls().done(function(locations){
+  //   console.log('app.js!!!!!!!!!!!!!! ' + locations)
+  // })
 
   function printEm(locations){
     console.log('app.js!!!!!!!!!!!!!! ' + locations)
   }
-  // locations.forEach(function(aLocation, index){
-    
-  //   if (index != locations.length -1){
-  //     db.Location.create({
-  //       url_yelp = aLocation.url_yelp,
-  //       name = aLocation.name,
-  //       review_count = aLocation.review_count;
-  //     })
-  //   } else {
-  //     db.Location.create({
-  //       url_yelp = aLocation.url_yelp,
-  //       name = aLocation.name,
-  //       review_count = aLocation.review_count;
-  //     }).then(function (){
-  //       db.Location.findAll({}).then(function(storedLocations){
-  //         res.render('index', {storedLocations:storedLocations})
-  //       })
-  //     })
-  //   }
-  // })
-
+  
+  getLocationUrls()
 
   // db.Location.findAll({}).then(function(Locations){
   // 	console.log(Locations[0].getReviews())
@@ -57,3 +46,89 @@ app.get("/", function (req, res) {
 app.listen(3000, function () {
   console.log("Starting a server on localhost:3000");
 });
+
+function getLocationUrls(){
+    
+      aLocation = {}
+      var locationUrls = []
+
+      if (searchResultsCounter == 0)
+        var url = baseUrl + addUrl1
+      else{
+        var addUrl2 = '&start=' + (searchResultsCounter * 10)// + multiple of 10 < 14480
+        var url = baseUrl + addUrl1 + addUrl2
+      }
+        
+
+      request(url, function (error, response, html) {
+        if (!error && response.statusCode == 200) {
+          var $ = cheerio.load(html);
+
+
+          // Saves each location's individual name and yelp url from 
+          // yelp search results
+
+          $('a.biz-name').each(function(i, element){
+            var a = $(this)
+            
+            aLocation['url_yelp'] = a.attr('href')
+            aLocation['name'] = a.html()
+            locations.push(aLocation)
+          })
+          
+          // Saves review count for each location
+
+          $('span.review-count').each(function(i, element){
+            var a = $(this)
+            
+            locations[i]['review_count'] = parseInt(a.html())
+            
+          })
+          
+
+          // If reaches last url on this page, proceeds to next list of
+          // search result urls. Otherwise, calls callback to explore each
+          // saved url's individual location page
+
+          if(searchResultsCounter < 1){
+           searchResultsCounter++;
+           // nextSearchUrls = $('span.current').parent().next('li').children('a').attr('href')
+           setTimeout(getLocationUrls, 1000)
+          } else {
+           console.log(locations)
+           writeLocDb();
+           // call(callback, true);
+          }
+          // call(callback);
+          
+
+        } else {
+          console.log(error);
+        }
+      
+
+    })
+}
+
+function writeLocDb(){
+  locations.forEach(function(aLocation, index){
+    
+    if (index != locations.length -1){
+      db.Location.create({
+        url_yelp : aLocation.url_yelp,
+        name : aLocation.name,
+        review_count : aLocation.review_count
+      })
+    } else {
+      db.Location.create({
+        url_yelp : aLocation.url_yelp,
+        name : aLocation.name,
+        review_count : aLocation.review_count
+      }).then(function (){
+        db.Location.findAll({}).then(function(storedLocations){
+          res.render('index', {storedLocations:storedLocations})
+        })
+      })
+    }
+  })
+}
