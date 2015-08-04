714742e8108dceafcfde1a95da3ef5fc37373ea2
Eduardo Moreno <emoreno@uchicago.edu>
2015-08-03 23:10:54 -0700

    unsuccessfully playing with promises...but not lying!

--- app.js
+++ app.js
@@ -3,20 +3,53 @@ var express = require('express'),
 
 var db = require('./models')
 
+var scrape = require('./scrape')
+
 app.set('view engine', 'ejs');
 
 // a "GET" request to "/" will run the function below
 app.get("/", function (req, res) {
   // send back the response: 'Hello World'
-  db.Location.findAll({}).then(function(Locations){
-  	console.log(Locations[0].getReviews())
-  	     Locations[0].getReviews().then(function (rev){ 
-  			 console.log(rev[0].date)  
-  			 rev[0].date  
-  			 res.render('index', {Locations:Locations, rev:rev});
-  	     }) 
+  
+
+  scrape.getLocationUrls().then(function(locations){
+    console.log('app.js!!!!!!!!!!!!!! ' + locations)
+  })
+
+  function printEm(locations){
+    console.log('app.js!!!!!!!!!!!!!! ' + locations)
+  }
+  // locations.forEach(function(aLocation, index){
+    
+  //   if (index != locations.length -1){
+  //     db.Location.create({
+  //       url_yelp = aLocation.url_yelp,
+  //       name = aLocation.name,
+  //       review_count = aLocation.review_count;
+  //     })
+  //   } else {
+  //     db.Location.create({
+  //       url_yelp = aLocation.url_yelp,
+  //       name = aLocation.name,
+  //       review_count = aLocation.review_count;
+  //     }).then(function (){
+  //       db.Location.findAll({}).then(function(storedLocations){
+  //         res.render('index', {storedLocations:storedLocations})
+  //       })
+  //     })
+  //   }
+  // })
+
+
+  // db.Location.findAll({}).then(function(Locations){
+  // 	console.log(Locations[0].getReviews())
+  // 	     Locations[0].getReviews().then(function (rev){ 
+  // 			 console.log(rev[0].date)  
+  // 			 rev[0].date  
+  // 			 res.render('index', {Locations:Locations, rev:rev});
+  // 	     }) 
   	
-  });
+  // });
 
 });
 
--- scrape.js
+++ scrape.js
@@ -5,44 +5,68 @@ var cheerio = require('cheerio');
 var baseUrl = 'http://www.yelp.com/search?find_desc=&find_loc=San+Francisco%2C+CA&ns=1#find_desc=food'
 var addUrl = '&start=' // + multiple of 10 < 14480
 var searchResultsCounter = 0;
+var locations = []
 
-function getLocationUrls(){
-	var locations = {}
-	var locationUrls = []
+var scrape = { 
 
-	request(baseUrl, function (error, response, html) {
-	  if (!error && response.statusCode == 200) {
-	    var $ = cheerio.load(html);
+	 getLocationUrls: function(){
+	 	return new Promise(function (fulfill, reject){
 
-	    $('a.biz-name').each(function(i, element){
-	    	var a = $(this)
-	    	locationUrls.push(a.attr('href'))	
-	    })
-	    
+	 		aLocation = {}
+	 		var locationUrls = []
 
-	    $('span.review-count').each(function(i, element){
-	    	var a = $(this)
-	    	// locArr.push(a.attr('href'))
+	 		request(baseUrl, function (error, response, html) {
+	 		  if (!error && response.statusCode == 200) {
+	 		    var $ = cheerio.load(html);
 
-	    	locationUrls[i].push(parseInt(a.html()))
-	    	// console.log(a.attr('href'))
-	    })
 
-	    locationUrls.push(diffLocArr);
+	 		    // Saves each location's individual name and yelp url from 
+	 		    // yelp search results
 
-	    if(searchResultsCounter < 1){
-	    	nextSearchUrls = $('span.current').parent().next('li').children('a').attr('href')
-	    	getUrls(call,true)
-	    } else {
-	    	console.log(locationUrls)
-	    	// call(callback, true);
-	    }
-	    // call(callback);
-	    
-	  } else {
-	  	console.log(error);
-	  }
-	});
+	 		    $('a.biz-name').each(function(i, element){
+	 		    	var a = $(this)
+	 		    	
+	 		    	aLocation['url_yelp'] = a.attr('href')
+	 		    	aLocation['name'] = a.html()
+	 		    	locations.push(aLocation)
+	 		    })
+	 		    
+	 		    // Saves review count for each location
+
+	 		    $('span.review-count').each(function(i, element){
+	 		    	var a = $(this)
+	 		    	
+	 		    	locations[i]['review_count'] = parseInt(a.html())
+	 		    	
+	 		    })
+	 		    console.log('scrape.js!!!!!!!!!!!!!! ' + locations)
+
+	 		    fulfill(locations);
+
+	 		    // If reaches last url on this page, proceeds to next list of
+	 		    // search result urls. Otherwise, calls callback to explore each
+	 		    // saved url's individual location page
+
+	 		    // if(searchResultsCounter < 1){
+	 		    // 	nextSearchUrls = $('span.current').parent().next('li').children('a').attr('href')
+	 		    // 	getUrls(call,true)
+	 		    // } else {
+	 		    // 	console.log(locations)
+	 		    // 	// call(callback, true);
+	 		    // }
+	 		    // call(callback);
+	 		    
+
+	 		  } else {
+	 		  	console.log(error);
+	 		  }
+	 		});
+
+	 	})
+		
+
+	}
 
 }
 
+module.exports = scrape;
